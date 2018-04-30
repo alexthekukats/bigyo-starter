fff00089656ea7c29775e512f5f3f1b60d7d117c
Kukac <thekukac9@gmail.com>
2018-05-01 00:55:14 +0200

    Initial commit

---
+++ bigyóstarter/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace bigyóstarter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Név = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Hely = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fájlnév = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Név,
+            this.Hely,
+            this.Fájlnév});
+            this.dataGridView1.DataSource = this.dataTableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(367, 167);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataTableBindingSource
+            // 
+            this.dataTableBindingSource.DataSource = typeof(System.Data.DataTable);
+            // 
+            // Név
+            // 
+            this.Név.HeaderText = "Név";
+            this.Név.Name = "Név";
+            // 
+            // Hely
+            // 
+            this.Hely.HeaderText = "Hely";
+            this.Hely.Name = "Hely";
+            // 
+            // Fájlnév
+            // 
+            this.Fájlnév.HeaderText = "Fájlnév";
+            this.Fájlnév.Name = "Fájlnév";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 392);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Név;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hely;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fájlnév;
+        private System.Windows.Forms.BindingSource dataTableBindingSource;
+    }
+}
+
---
+++ bigyóstarter/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bigyóstarter
+{
+    public partial class Form1 : Form
+    {
+        struct Records
+        {
+            public string forras;
+            public string fajlnev;
+            public string nev;
+        }
+
+
+        static DataTable GetTable()
+        {
+            //
+            // Here we create a DataTable with four columns.
+            //
+            DataTable table = new DataTable();
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            //
+            // Here we add five DataRows.
+            //
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+            return table;
+        }
+
+
+        //public List<Records> elérési_út = new List<Records>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            string filename;
+            string path;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filename = Path.GetFileName(ofd.FileName);
+                path = Path.GetDirectoryName(ofd.FileName);
+            }
+        }
+    }
+}
